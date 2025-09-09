9cfcf89a8a2338bda7b7d8ca564f1fe2d3c18b6f
sachit24 <sachitsapra@gmail.com>
2025-09-09 12:58:31 -0400

    Overloaded Multiples

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -15,4 +15,13 @@ public class Multiples {
         }
         return multiplesofab;
     }
+
+    public static int multiples() {
+        int multiplesofab = 0;
+        for(int i=1;i<1000;i++)
+        {
+            if(i%3==0 || i%5==0) multiplesofab++;
+        }
+        return multiplesofab;
+    }
 }
